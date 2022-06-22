b9337cba1ad803f5826ffac6c7e9e9a60458fa73
yoty19 <97879925+yoty19@users.noreply.github.com>
2022-06-22 09:31:52 +0300

    fixed function

--- assets/index.js
+++ assets/index.js
@@ -2,13 +2,16 @@ const input = document.getElementById(`xNum`);
 const result = document.getElementById(`yNum`);
 const isBtn = document.querySelector(`.btn`);
 
-function btnClicked() {
+const btnClick = () => {
     const x = input.value;
     fetch(`http://localhost:5050/fibonacci/${x}`)
         .then(Response => Response.json())
         .then(data => {
             result.innerText = data.result;
         })
+        .catch(err => {
+            result.innerText = ``;
+        })
 }
 
-isBtn.addEventListener(`click`, btnClicked);
+isBtn.addEventListener(`click`, btnClick);
